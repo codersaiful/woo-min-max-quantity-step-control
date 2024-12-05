b5b36ddb3422aa34e33295d258e6409be3ed0ea5
Saiful Islam <codersaiful@gmail.com>
2024-12-05 13:07:54 +0600

    version updated

--- wcmmq.php
+++ wcmmq.php
@@ -10,7 +10,7 @@
  * 
  * Version: 6.5
  * Requires at least:    4.0.0
- * Tested up to:         6.5.3
+ * Tested up to:         6.7.1
  * WC requires at least: 3.0.0
  * WC tested up to: 	 9.3.3
  * 
